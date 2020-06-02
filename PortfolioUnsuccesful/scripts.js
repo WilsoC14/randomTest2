e37adb8a04378897390a83d23ff02e722a0f8a5f
Casey Wilson <caswil10@gmail.com>
2020-06-02 19:11:10 -0400

    Initial Commit

---
+++ PortfolioUnsuccesful/scripts.js
@@ -0,0 +1,17 @@
+function ClearDetails() {
+    var element = document.getElementsByClassName("details");
+    element.innerHTML = " cleared"
+}
+
+function AboutToggle() {
+    ClearDetails();
+    var element = document.getElementById("About");
+    element.innerHTML = "<h3>About Me</h3> <br> <p>  About Me, About Me about me about me about me About Me, About Me about me about me about me About Me, About Me about me about me about me About Me, About Me about me about me about me About Me, About Me about me about me about me About Me, About Me about   me about me about me About Me, About Me about me about me about me About Me, About Me about me about me about me About Me, About Me about me about me about me About Me, About Me about me about me about me About Me, About Me about me   about me about me About Me, About Me about me about me about me About Me, About Me about me about me about me About Me, About Me about me about me about me About Me, About Me about me about me about me About Me, About Me about me about   me about me About Me, About Me about me</p><p>    What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type  specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,  and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type</p><p>   What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type  specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,  and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>"
+
+}
+
+function ProjectsToggle() {
+    var element = document.getElementById("Projects");
+    element.innerHTML = "<p>Projects projects projects projects projects Projects projects projects projects projects projects Projects projects projects projects projects projects Projects projects projects projects projects projects Projects projects projects projects projectsprojects Projects projects projects projects projects projects Projects projects projects projects projects projects Projects projects projects projects projects projects Projects projects projects projects projects projects Projectsprojects projects projects projects projects Projects projects projects projects projects projects Projects projects projects projects projects projects</p>"
+
+}
